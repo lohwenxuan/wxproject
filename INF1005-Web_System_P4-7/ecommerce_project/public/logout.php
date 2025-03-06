fec2001f013b8631b3b6b70535ee18a1a94700ab
Loh Wen Xuan <2401561@sit.singaporetech.edu.sg>
2025-03-07 03:17:57 +0800

    Delete logout.php

--- INF1005-Web_System_P4-7/ecommerce_project/public/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); // Remove session variables
-session_destroy(); // Destroy the session
-
-header("Location: login.php"); // Redirect to login page after logout
-exit();
-?>
