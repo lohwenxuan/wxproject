1cd55c0ac0f828b64c6c43b7ab9791ff180ba78f
Loh Wen Xuan <2401561@sit.singaporetech.edu.sg>
2025-03-12 11:36:35 +0800

    Update index.php

--- ecommerce_project/public/index.php
+++ ecommerce_project/public/index.php
@@ -25,68 +25,14 @@
         </div>
     </div>
 
-    <main class="container">
-    <?php
-    include __DIR__ . "/../inc/products.inc.php";
-    ?>
-        <!-- First Section -->
-        <section id="dogs">
-            <h2>All About Dogs!</h2>
-            <div class="row">
-                <article class="col-sm">
-                    <h3>Poodle</h3>
-                    <figure>
-                        <img src="images/poodle_small.jpg" popup-src="images/poodle_large.jpg" alt="Poodle" class="img-thumbnail" title="View larger image..." />
-                        <figcaption>Standard Poodle</figcaption>
-                    </figure>
-                    <p>
-                        Poodles are a group of formal dog breeds, the Standard Poodle,
-                        Miniature Poodle and Toy Poodle.
-                    </p>
-                </article>
-                <article class="col-sm">
-                    <h3>Chihuahua</h3>
-                    <figure>
-                        <img src="images/chihuahua_small.jpg" popup-src="images/chihuahua_large.jpg" alt="Poodle" class="img-thumbnail" title="View larger image..." />
-                        <figcaption class="center">Chihuahua</figcaption>
-                    </figure>
-                    <p>
-                        The Chihuahua is the smallest breed of dog, and is named after the
-                        Mexican state of Chihuahua.
-                    </p>
-                </article>
-            </div>
-        </section>
+   
 
-        <!-- Second Section -->
-        <section id="cats">
-            <h2>All About Cats!</h2>
-            <div class="row">
-                <article class="col-sm">
-                    <h3>Calico</h3>
-                    <figure>
-                        <img src="images/calico_small.jpg" popup-src="images/calico_large.jpg" alt="Poodle" class="img-thumbnail" title="View larger image..." />
-                        <figcaption>Standard Calico</figcaption>
-                    </figure>
-                    <p>
-                        A calico cat (US English) is a domestic cat of any breed with a
-                        tri-color coat.
-                    </p>
-                </article>
-                <article class="col-sm">
-                    <h3>Tabby</h3>
-                    <figure>
-                        <img src="images/tabby_small.jpg" popup-src="images/tabby_large.jpg" alt="Poodle" class="img-thumbnail" title="View larger image..." />
-                        <figcaption>Tabby</figcaption>
-                    </figure>
-                    <p>
-                        A tabby cat, or simply tabby, is any domestic cat (Felis catus)
-                        with a distinctive M-shaped marking on its forehead, stripes by
-                        its eyes and across its cheeks
-                    </p>
-                </article>
-            </div>
-        </section>
+    <main class="container">
+         <!-- Featured Products -->
+        <?php
+        include __DIR__ . "/../inc/products.inc.php";
+        ?>
+       
     </main>
     <?php
     include __DIR__ . "/../inc/footer.inc.php";
